c8ddc5628b876b8fd440627a9ea5beb1da474a8a
Angelo Longoria <arlongoria93@gmail.com>
2021-07-16 18:49:39 -0500

    git init

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+// tailwind.config.js
+const colors = require("tailwindcss/colors");
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    // ...
+  ],
+};
