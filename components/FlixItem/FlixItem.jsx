6b3e6272fa43e3b99231e4840e0314839fed4ebb
Angelo Longoria <arlongoria93@gmail.com>
2021-07-26 15:39:50 -0500

    added different react slider to project

--- components/FlixItem/FlixItem.jsx
+++ components/FlixItem/FlixItem.jsx
@@ -3,17 +3,19 @@ import Link from "next/link";
 
 const FlixItem = (props) => {
   return (
-    <Link href={"/anime/" + props.id}>
-      <a>
-        <Image
-          src={props.img}
-          width={200}
-          height={300}
-          objectFit="cover"
-          className="z-0 rounded"
-        />
-      </a>
-    </Link>
+    <div className="p-2">
+      <Link href={"/anime/" + props.id}>
+        <a>
+          <Image
+            src={props.img}
+            width={200}
+            height={300}
+            objectFit="cover"
+            className="z-0 rounded "
+          />
+        </a>
+      </Link>
+    </div>
   );
 };
 
--- components/FlixRow/FlixRow.jsx
+++ components/FlixRow/FlixRow.jsx
@@ -1,106 +1,40 @@
 import React from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper.min.css";
-import "swiper/components/pagination/pagination.min.css";
-import "swiper/components/navigation/navigation.min.css";
-import styles from "./FlixRow.module.css";
 
-import SwiperCore, { Pagination, Navigation } from "swiper/core";
+import styles from "./FlixRow.module.css";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 import FlixItem from "../FlixItem/FlixItem";
 
-// install Swiper modules
-SwiperCore.use([Navigation, Pagination]);
 const FlixRow = (props) => {
   const { media, nav } = props;
 
   const test = media.map((anime) => {
-    return (
-      <SwiperSlide key={anime.id}>
-        <FlixItem img={anime.coverImage.large} id={anime.id} />
-      </SwiperSlide>
-    );
+    return <FlixItem img={anime.coverImage.large} id={anime.id} />;
   });
 
-  const next = `next${props.nav}`;
-  const prev = `prev${props.nav}`;
   return (
-    <>
-      {/* Small Screen Slider */}
-      <div className="flex justify-between items-center mb-2">
+    <div className="flex flex-col">
+      <div>
         <div>
-          <h1 className="font-display text-xl text-white">{props.title}</h1>
+          <h1 className="font-display text-white">{props.title}</h1>
         </div>
-        <div className="flex items-center justify-between">
-          <button className={prev}>
-            <div className="transition text-white duration-500 p-1  hover:text-secondaryBG">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M10 19l-7-7m0 0l7-7m-7 7h18"
-                />
-              </svg>
-            </div>
-          </button>
-
-          <button className={next}>
-            {" "}
-            <div className="transition text-white duration-500  hover:text-secondaryBG">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 "
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M14 5l7 7m0 0l-7 7m7-7H3"
-                />
-              </svg>
-            </div>
-          </button>
-        </div>
+        <div></div>
       </div>
-
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={10}
-        navigation={{
-          nextEl: `.next${props.nav}`,
-          prevEl: `.prev${props.nav}`,
-        }}
-        className=" lg:hidden"
-      >
-        {test}
-      </Swiper>
-      <Swiper
-        slidesPerView={8}
-        spaceBetween={10}
-        navigation={{
-          nextEl: `.next${props.nav}`,
-          prevEl: `.prev${props.nav}`,
-        }}
-        className=" iphone:hidden lg:block"
-      >
-        {test}
-      </Swiper>
-    </>
+      <div>
+        <AliceCarousel
+          mouseTracking
+          items={test}
+          disableButtonsControls
+          disableDotsControls
+          responsive={{
+            319: { items: 3 },
+            768: { items: 6 },
+            1024: { items: 9 },
+          }}
+        />
+      </div>
+    </div>
   );
 };
-{
-  /* <SwiperSlide>
-          <FlixItem img={"/images/cover/cover.jpg"} id={"ANIME"} />
-        </SwiperSlide> */
-}
+
 export default FlixRow;
