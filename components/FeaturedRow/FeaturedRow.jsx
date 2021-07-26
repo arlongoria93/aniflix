15e0b1b39acc25f45a87c7ff4426244c89f46036
Angelo Longoria <arlongoria93@gmail.com>
2021-07-25 21:42:50 -0500

    mobile test 3

--- components/FeaturedRow/FeaturedRow.jsx
+++ components/FeaturedRow/FeaturedRow.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./FeaturedRow.module.css";
-const sectionContainer = `${styles.bg} bg-mainBG`;
+const sectionContainer = `${styles.bg} bg-mainBG overflow-hidden`;
 const FeaturedRow = () => {
   const s = `Young Thorfinn grew up listening to the stories of old sailors
                 that had traveled the ocean and reached the place of legend,
@@ -13,35 +13,27 @@ const FeaturedRow = () => {
                 further from the truth.`;
   return (
     <section className={sectionContainer}>
-      <div className="relative h-full bg-mainBG bg-blend-multiply">
-        {" "}
-        <div className="flex items-center text-center w-full h-full absolute  z-10">
-          <div className=" grid grid-cols-1 lg:w-1/2 sm:w-full  m:w-full sm:p-4 md:p-6 lg:p-10">
-            <div>
-              <h1 className="font-display text-white text-left  font-bold text-xl lg:text-2xl">
-                Vinland Saga
-              </h1>
-              <br className="border border-black w-full " />
-              <p className="text-sm leading-8 text-white font-display lg:text-left lg:text-lg lg:leading-10">
-                {s}
-              </p>
-            </div>
-            <div>
-              <button className="transition duration-300 bg-secondaryBG hover:bg-brightBG text-white w-1/5 mt-10 font-display p-1 cursor-pointer">
-                Learn More
-              </button>
-            </div>
-          </div>
+      <div className="grid grid-cols-1">
+        <div className="w-1/2 justify-self-center">
+          <Image
+            src="/images/cover/cover.jpg"
+            className="object-contain"
+            width={460}
+            height={650}
+          />
         </div>
-        <div className="z-0 ">
-          <div className="absolute h-full w-full bg-blend-hue bottom-0 left-0">
-            <Image
-              src="/images/featured/featured.jpg"
-              layout="fill"
-              objectFit="cover"
-              className=" opacity-30 z-0 sm:object-center lg:object-top"
-            />
-          </div>
+        <div className="font-display overflow-y-auto text-white">
+          <p className="iphone:text-sm overflow-y-auto">
+            Young Thorfinn grew up listening to the stories of old sailors that
+            had traveled the ocean and reached the place of legend, Vinland.
+            It's said to be warm and fertile, a place where there would be no
+            need for fighting—not at all like the frozen village in Iceland
+            where he was born, and certainly not like his current life as a
+            mercenary. War is his home now. Though his father once told him,
+            "You have no enemies, nobody does. There is nobody who it's okay to
+            hurt," as he grew, Thorfinn knew that nothing was further from the
+            truth.
+          </p>
         </div>
       </div>
     </section>
