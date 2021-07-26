39b16cb618058083b79d852c101159981212f019
Angelo Longoria <arlongoria93@gmail.com>
2021-07-25 21:18:14 -0500

    s

--- components/FeaturedRow/FeaturedRow.jsx
+++ components/FeaturedRow/FeaturedRow.jsx
@@ -1,10 +1,16 @@
+import Image from "next/image";
 import styles from "./FeaturedRow.module.css";
-const sectionContainer = `${styles.bg} bg-mainBG grid iphone:gird-cols-1 p-4 gap-4`;
+const sectionContainer = `${styles.bg} overflow-hidden bg-mainBG grid iphone:gird-cols-1 p-4 gap-4`;
 const FeaturedRow = () => {
   return (
     <section className={sectionContainer}>
-      <div className="w-1/2 justify-self-center">
-        <img src="/images/cover/cover.jpg" className="object-contain" />
+      <div className="w-1/2 justify-self-center flex">
+        <Image
+          src="/images/cover/cover.jpg"
+          className="object-contain"
+          width={460}
+          height={650}
+        />
       </div>
       <div className="  overflow-y-auto font-display text-white">
         <p className="iphone:text-sm ">
