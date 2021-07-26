9db9753d80b8ef970200a1ecf2e3ca029fb9257e
Angelo Longoria <arlongoria93@gmail.com>
2021-07-25 21:19:27 -0500

    testing error in build

--- components/FeaturedRow/FeaturedRow.jsx
+++ components/FeaturedRow/FeaturedRow.jsx
@@ -13,13 +13,7 @@ const FeaturedRow = () => {
         />
       </div>
       <div className="  overflow-y-auto font-display text-white">
-        <p className="iphone:text-sm ">
-          Thorfinn is son to one of the Vikings' greatest warriors, but when his
-          father is killed in battle by the mercenary leader Askeladd, he swears
-          to have his revenge. Thorfinn joins Askeladd's band in order to
-          challenge him to a duel, and ends up caught in the middle of a war for
-          the crown of England.
-        </p>
+        <p className="iphone:text-sm"></p>
       </div>
     </section>
   );
